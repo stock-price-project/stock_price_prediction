561de4255eed41c1797d50f327c766c88dcb43a7
Rajat Sharma <rajatsharma369007@gmail.com>
2019-02-22 03:46:08 +0530

    Updated Test section
    some modification code is done, in order to predict using 2 feature attribute

--- lstm.py
+++ lstm.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import r2_score, mean_squared_error
 
 # Importing the training set
 df = pd.read_csv('train.csv')
@@ -83,52 +84,59 @@ print("Loaded model from disk")
 ###############################################################################
 
 # importing the testing file
-test_data = pd.read_csv('test.csv')
-actual_open = test_data.iloc[:, 1:2].values                 # from 0 to 102
+df_test = pd.read_csv('test.csv')
+actual_open = df_test.iloc[:, [1, 4]].values
 
-dataset_total = pd.concat((df['Open'], test_data['Open']), axis=0)
-inputs = dataset_total[len(dataset_total) - len(test_data) - 60:].values
-inputs = inputs.reshape(-1,1)
-inputs = sc.transform(inputs)
+# feature scaling
+sc_t = MinMaxScaler(feature_range = (0,1))
+actual_open_scaled = sc_t.fit_transform(actual_open)
 
+# creating test data
 X_test = []
-for i in range(60, len(actual_open)+60):
-    X_test.append(inputs[i-60:i, 0])
+for i in range(60, len(actual_open_scaled)):
+    X_test.append(actual_open_scaled[i-60: i])
+    
+# converting to numpy array
 X_test = np.array(X_test)
-X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 
-pred_open = loaded_model.predict(X_test)
-pred_open_train = loaded_model.predict(X_train)
+# creating 3D tensor
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 2))    
 
-pred_open = sc.inverse_transform(pred_open)
-pred_open_train = sc.inverse_transform(pred_open_train)
+# performing prediction on test set
+pred_test_scaled = loaded_model.predict(X_test)
+# performing prediction on train set
+pred_train_scaled = loaded_model.predict(X_train)
 
-from sklearn.metrics import r2_score, mean_squared_error
+# rescaling for predictions ( test data )
+scpred = MinMaxScaler(feature_range = (0,1))
+scpred = scpred.fit(actual_open[:,0].reshape(-1,1))
+pred_test = scpred.inverse_transform(pred_test_scaled)
 
-print(r2_score(actual_open, pred_open))
-print(mean_squared_error(actual_open, pred_open))
+# rescaling for predictions ( train data )
+scpred1 = MinMaxScaler(feature_range = (0,1))
+scpred1 = scpred1.fit(training_set[:,0].reshape(-1,1))
+pred_train = scpred1.inverse_transform(pred_train_scaled)
 
-# visualising the results
-plt.plot(actual_open , color='red', label='actual stock price')
-plt.plot(pred_open, color='blue', label='predicted stock price')
+# r2 score and mse score on test data
+print(r2_score(actual_open[60:len(actual_open),0], pred_test))
+print(mean_squared_error(actual_open[60:len(actual_open),0], pred_test))
+
+# visualising the results for test results
+plt.plot(actual_open[60:len(actual_open),0] , color='red', label='actual stock price')
+plt.plot(pred_test, color='blue', label='predicted stock price')
 plt.title('google stock price')
 plt.xlabel('time')
 plt.ylabel('google stock price')
 plt.legend()
 plt.show()
 
+# r2 score and mse score on train data
+print(r2_score(training_set[60:len(training_set),0], pred_train))
+print(mean_squared_error(training_set[60:len(training_set),0], pred_train))
 
-y_train = sc.inverse_transform(y_train.reshape(-1,1))
-z = np.append(pred_open_train, pred_open)
-w = np.append(y_train, actual_open)
-
-print(r2_score(w, z))
-print(mean_squared_error(w, z))
-
-
-# visualising the results
-plt.plot(w, color='red', label='actual stock price')
-plt.plot(z, color='blue', label='predicted stock price')
+# visualising the results for train results
+plt.plot(training_set[60:len(training_set),0], color='red', label='actual stock price')
+plt.plot(pred_train, color='blue', label='predicted stock price')
 plt.title('google stock price')
 plt.xlabel('time')
 plt.ylabel('google stock price')
