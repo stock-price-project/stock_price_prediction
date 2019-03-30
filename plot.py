361249907f26ba28e2877b1733769f1a2a41d4a0
Rajat Sharma <rajatsharma369007@gmail.com>
2019-03-30 12:57:05 +0530

    implemented plotting script

---
+++ plot.py
@@ -0,0 +1,14 @@
+'''
+This script is for plotting the time series graph
+'''
+
+import matplotlib.pyplot as plt
+
+def time_series_plot(actual, predict, actual_color, predict_color, label_actual, label_predict, xlabel, ylabel, title):
+    plt.plot(actual , color= actual_color, label = label_actual)
+    plt.plot(predict, color= predict_color, label = label_predict)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.legend(loc='best')
+    plt.show()    
